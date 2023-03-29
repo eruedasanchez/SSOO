f18b0cba07e0e844aacb37184b74dbae7eca98f0
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-03-28 22:17:48 -0300

    Kill a process apply the syscall SIGKILL

---
+++ clases-practicas/clase-1/kill.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int main(){
+    pid_t pid = fork();
+    if(pid == 0){
+        printf("%s\n", "Soy el hijo y estoy colgado");
+        while(1){}
+    } else {
+        printf("%s\n", "Soy el padre y estoy esperando a:", pid);
+        kill(pid, SIGKILL);                                             // Con esto, se logra que el proceso padre finalice o mate al proceso hijo 
+        wait(NULL);
+        printf("%s\n", "Mi hijo ya termino, ahora me toca a mi");
+    }
+    return 0;
+}
+
+
+
